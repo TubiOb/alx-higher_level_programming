a03a2be28b15d2b226681e3dc6e4c1c75b882e87
TubiOb <tubiobaloluwa@gmail.com>
2022-06-02 00:28:59 -0700

    If/else, Loops commit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if last_digit > 5:
     print(f"{message} and is greater than 5")
 elif last_digit == 0:
     print(f"{message} and is 0")
-elif last_digit < 6 && last_digit != 0:
+elif last_digit < 6 and last_digit != 0:
     print(f"{message} and is less than 6 and not 0")
