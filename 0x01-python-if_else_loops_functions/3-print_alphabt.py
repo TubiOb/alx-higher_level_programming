b378ac440175d692dda1dc9c945e6e9ffa208852
TubiOb <tubiobaloluwa@gmail.com>
2022-06-02 00:22:05 -0700

    If/else, Loops commit

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for char in range(26):
-    if char != 4 and char != 16:
-        print("{:s}".format(chr(char + ord("a"))), end = "")
+for i in range(ord('a'), ord('z') + 1:
+        if i == ord('e') or i == ord('q'):
+            continue
+        print("{:c}".format(i), end="")
