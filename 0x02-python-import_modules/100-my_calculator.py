24b26df5d6cf9631e317359af3bee9e2eb923766
TubiOb <tubiobaloluwa@gmail.com>
2022-06-02 11:28:12 -0700

    Import and Modules commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,11 +4,9 @@ if __name__ == "__main__":
     import sys
 
      num_args = len(sys.argv)
-
      if num_args != 4:
          print("Usage: ./100-my_calculator.py <a> <operator> <b>")
          exit(1)
-
     a = int(sys.argv[1])
     op = sys.argv[2]
     b = int(sys.argv[3])
