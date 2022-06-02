6a1a6915d717b0f913f1fbd2b42cd16c64ce967a
TubiOb <tubiobaloluwa@gmail.com>
2022-06-02 11:35:29 -0700

    Import and Modules commit

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
+from sys import argv
+
 if __name__ == "__main__":
-    from sys import argv
-    num = len(sys.argv)
-    if num == 1:
-        print("{} arguments.".format(num - 1))
-    elif num == 2:
-        print("{} argument:".format(num - 1))
-    else:
-        print("{} arguments:".format(num - 1))
+    i = 1
+    args = len(argv) - 1
 
-
-    for i in range(1, num):
-        print("{}: {}".format(i, sys.argv[i]))
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
+    print(f"{'.' if args == 0 else ':'}")
+    while (i <= args):
+        print(f"{i}: {argv[i]}")
+        i += 1
